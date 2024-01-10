40660d9f456fd9cdd994f585bb0fa734915cefdf
Emmanuel <emmanuelsuareztou@gmail.com>
2024-01-10 16:00:05 -0300

    Update JSON and TopBar styling

--- src/star/src/Components/TopBar.tsx
+++ src/star/src/Components/TopBar.tsx
@@ -9,9 +9,14 @@ function TopBar() {
           <div className="row-left"></div>
           <div className="row-left"></div>
         </div>
-        <Link to={"/star"}>
-          <h1 className="title center">tenzo.star</h1>
-        </Link>
+        <div className="topBar-title-container">
+          <Link to={"/star"}>
+            <h1 className="title center">tenzo.star</h1>
+          </Link>
+          <Link to={"/home"}>
+            <h1 className="title tech">back to tenzo.tech</h1>
+          </Link>
+        </div>
         <div className="flexCol">
           <div className="row-right"></div>
           <div className="row-right"></div>
