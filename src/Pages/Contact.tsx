e12559f5c1da06fef014f76e78769f614d74e121
Emmanuel <emmanuelsuareztou@gmail.com>
2024-03-25 11:44:25 -0300

    Remove userContext and update routing in main.tsx

--- src/Pages/Contact.tsx
+++ src/Pages/Contact.tsx
@@ -1,11 +1,7 @@
 import { Navbar, Footer } from "../Components";
-import { useContext } from "react";
-import { UserContext } from "../Context/userContext";
 import cv from "../assets/static/CV_EmmanuelSuarez.pdf";
 import "../styles/CSS/Contact.css";
 function Contact() {
-  const { user } = useContext(UserContext);
-
   return (
     <>
       <Navbar color="gradient-green" />
@@ -14,7 +10,7 @@ function Contact() {
         <h2>
           Here you have all the ways to get in touch with me; my socials,
           <br /> my email and also my resume. Hope to hear from you soon{" "}
-          <span>{user}</span>!
+          <span>friend</span>!
         </h2>
 
         <section className="socials-wrapper">
--- src/main.tsx
+++ src/main.tsx
@@ -3,20 +3,17 @@ import ReactDOM from "react-dom/client";
 import { Contact, Landing, ProjectDetail, Projects, AboutMe } from "./Pages";
 import "./styles/CSS/reset.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { UserProvider } from "./Context/userContext";
 
 const App = () => {
   return (
     <Router>
-      <UserProvider>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/about" element={<AboutMe />} />
-          <Route path="/project/:id" element={<ProjectDetail />} />
-        </Routes>
-      </UserProvider>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<AboutMe />} />
+        <Route path="/project/:id" element={<ProjectDetail />} />
+      </Routes>
     </Router>
   );
 };
